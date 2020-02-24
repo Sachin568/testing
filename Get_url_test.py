d844eda9b31873a6a6a7f9b412eb50b003aa07c2
Sachin Paramesha <45459128+Sachin568@users.noreply.github.com>
2020-02-24 17:13:45 -0500

    Add files via upload

--- Get_url_test.py
+++ Get_url_test.py
@@ -7,11 +7,11 @@ class TestGetRepo(unittest.TestCase):
     def test_normal_response(self):
         expected = ['User: Sachin568', 'Repo: Django Number of commits: 1', 'Repo: embedded-projects Number of commits: 2', 'Repo: helloworld Number of commits: 1',
                     'Repo: Image-Processing Number of commits: 2', 'Repo: IoT-automation Number of commits: 2', 'Repo: Python_Repository Number of commits: 6',
-                    'Repo: testing Number of commits: 18', 'Repo: Web-controlled-robot Number of commits: 2', 'Repo: web-_programming Number of commits: 2']
+                    'Repo: testing Number of commits: 19', 'Repo: Web-controlled-robot Number of commits: 2', 'Repo: web-_programming Number of commits: 2']
         self.assertEqual(get_repo_info(), expected)
 
-#     def test_bad_user_name(self):
-#         self.assertEqual(get_repo_info(), 'unable to fetch repos from user')
+    # def test_bad_user_name(self):
+    #     self.assertEqual(get_repo_info(), 'unable to fetch repos from user')
         
 
 
