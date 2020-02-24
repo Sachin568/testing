5ce43eb0e2a8a73ff2d948cd7ceccfbc35cd41fa
Sachin Paramesha <45459128+Sachin568@users.noreply.github.com>
2020-02-24 15:29:42 -0500

    Add files via upload

--- Get_url_test.py
+++ Get_url_test.py
@@ -5,15 +5,13 @@ from Get_url import get_repo_info
 
 class TestGetRepo(unittest.TestCase):
     def test_normal_response(self):
-        expected = ['User: Sachin568', 'Repo: Django Number of commits: 1', 'Repo: embedded-projects Number of commits: 2',
-                    'Repo: helloworld Number of commits: 1', 'Repo: Image-Processing Number of commits: 2',
-                    'Repo: IoT-automation Number of commits: 2', 'Repo: Python_Repository Number of commits: 6',
-                    'Repo: testing Number of commits: 10', 'Repo: Web-controlled-robot Number of commits: 2', 
-                    'Repo: web-_programming Number of commits: 2']
+        expected = ['User: Sachin568', 'Repo: Django Number of commits: 1', 'Repo: embedded-projects Number of commits: 2', 'Repo: helloworld Number of commits: 1',
+                    'Repo: Image-Processing Number of commits: 2', 'Repo: IoT-automation Number of commits: 2', 'Repo: Python_Repository Number of commits: 6',
+                    'Repo: testing Number of commits: 15', 'Repo: Web-controlled-robot Number of commits: 2', 'Repo: web-_programming Number of commits: 2']
         self.assertEqual(get_repo_info(), expected)
 
     def test_bad_user_name(self):
-        self.assertEqual(get_repo_info(), 'unable to fetch commits from user')
+        self.assertEqual(get_repo_info(), 'unable to fetch repos from user')
         
 
 
